f6bc8b12862d1767586544f248c47807c269f17c
Nathael Arki <nathael.arki@supinternet.fr>
2017-05-24 17:32:25 +0200

    foler recusrive

--- Model/FoldersManager.php
+++ Model/FoldersManager.php
@@ -34,6 +34,7 @@ class FoldersManager{
     public function checkCreateFolder($folder_name, $container)
     {
         $errors = array();
+
         return $errors;
     }
 
@@ -46,12 +47,21 @@ class FoldersManager{
             $folder['container_id'] = 0;
         }
         else{
-            $container_folder_path = $this->DBManager->findOneSecure("SELECT * FROM folders WHERE user_id = :user_id AND id = :folder_id", ['user_id' => $_SESSION['user_id'], 'id' => $container]);
-            $folder['folderpath'] = $container_folder_path . '/' . $folder_name;
+            $container_folder_path = $this->DBManager->findOneSecure("SELECT * FROM folders WHERE user_id = :user_id AND id = :folder_id", ['user_id' => $_SESSION['user_id'], 'folder_id' => $container]);
+            $folder['folderpath'] = $container_folder_path['folderpath'] . '/' . $folder_name;
             $folder['user_id'] = $_SESSION['user_id'];
             $folder['container_id'] = $container;
         }     
         $this->DBManager->insert('folders', $folder);
+        $data = $this->DBManager->findOneSecure("SELECT * FROM folders WHERE user_id = :user_id AND foldername = :foldername", ['user_id' => $_SESSION['user_id'], 'foldername' => $folder['foldername']]);
+        if($container == 0){
+            $new_path = 'uploads/' . $_SESSION['user_id'] . '/' . $data['id'];
+        }
+        else{
+            $new_path= $container_folder_path['folderpath'] . '/' . $data['id'];
+        }  
+       $update = $this->DBManager->findOneSecure("UPDATE folders SET folderpath = :folderpath where user_id = :user_id AND id = :folder_id", ['folderpath' => $new_path, 'user_id' => $_SESSION['user_id'], 'folder_id' => $data['id']]);
+       mkdir($new_path);
     }
 
 }
