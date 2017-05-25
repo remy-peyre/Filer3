21d7f435664d71d48d39790a1e663178f2567676
remy peyre <remy.peyre05@gmail.com>
2017-05-25 16:42:24 +0200

    modif colum left and chande js with form rename

--- web/script/script.js
+++ web/script/script.js
@@ -1,11 +1,27 @@
 window.onload = function () {
+    console.log('coucou');
     var renameButton = document.getElementsByClassName("rename_button");
 
     for (var i = 0; i< renameButton.length; i++ ){
         renameButton[i].onclick = function () {
-            this.parentNode.parentNode.childNodes[11].classList.remove("none");
-            this.parentNode.parentNode.childNodes[11].className = "form_rename";
-            this.parentNode.parentNode.childNodes[11].style.display = 'block';
+            this.parentNode.parentNode.childNodes[7].classList.remove("none");
+            this.parentNode.parentNode.childNodes[7].className = "form_rename";
+            this.parentNode.parentNode.childNodes[7].style.display = 'block';
+            /*console.log(this.parentNode.parentNode.childNodes[5]);
+            console.log(this.parentNode.parentNode.childNodes[6]);
+            console.log(this.parentNode.parentNode.childNodes[7]);
+            console.log(this.parentNode.parentNode.childNodes[7]);*/
         }
     }
+
+    var renameFolderButton = document.getElementsByClassName("rename_folder_button");
+
+    for (var i = 0; i< renameFolderButton.length; i++ ){
+        renameFolderButton[i].onclick = function () {
+            this.parentNode.parentNode.childNodes[7].classList.remove("none");
+            this.parentNode.parentNode.childNodes[7].className = "form_rename_folder";
+            this.parentNode.parentNode.childNodes[7].style.display = 'block';
+        }
+    }
+
 }
